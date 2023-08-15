2ed52b85a7fef97fade3517186b0e450d2323171
Dung Ha <dungha.tien@outlook.com>
2023-08-15 22:04:22 +0700

    update

--- tests/test_cases/test_allure.py
+++ tests/test_cases/test_allure.py
@@ -17,3 +17,15 @@ def test_skip():
 
 def test_broken():
     raise Exception('oops')
+
+
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_xfail_expected_failure():
+    """this test is an xfail that will be marked as expected failure"""
+    assert False
+
+
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_xfail_unexpected_pass():
+    """this test is an xfail that will be marked as unexpected success"""
+    assert True
